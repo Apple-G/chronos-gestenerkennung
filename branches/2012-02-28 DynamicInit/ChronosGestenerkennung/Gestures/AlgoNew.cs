ade92f8a17f9dcbebbb28354dc76bdd7b9ee2a7e
stefan.kraus05 <stefan.kraus05@9faf476d-1396-61c0-b798-1c026b2d322d>
2012-02-28 20:33:43 +0000

    included median into algorithm configured up down left right push with some bugs

--- branches/2012-02-28 DynamicInit/ChronosGestenerkennung/Gestures/AlgoNew.cs
+++ branches/2012-02-28 DynamicInit/ChronosGestenerkennung/Gestures/AlgoNew.cs
@@ -8,14 +8,14 @@ namespace ChronosGestenerkennung.Gestures
     class AlgoNew
     {
         private readonly int arraySize;
-
+        private int MedianDiffValue { get { return 10; } }
         CalcValues valueX;
         CalcValues valueY;
         CalcValues valueZ;
 
         public AlgoNew()
         {
-            arraySize = 10;
+            arraySize = 30;
             valueX = new CalcValues(arraySize);
             valueY = new CalcValues(arraySize);
             valueZ = new CalcValues(arraySize);
@@ -50,14 +50,53 @@ namespace ChronosGestenerkennung.Gestures
                 ResetValues();
                 return GestureType.Up;
             }
+
+            if (isDown())
+            {
+                Console.WriteLine("Down!!!");
+                ResetValues();
+                return GestureType.Down;
+            }
+            if (isLeft())
+            {
+                Console.WriteLine("Left!!!");
+                ResetValues();
+                return GestureType.Left;
+            }
+            if (isRight())
+            {
+                Console.WriteLine("Right!!!");
+                ResetValues();
+                return GestureType.Right;
+            }
+
             return GestureType.None;
         }
 
         private bool isUp()
         {
-            if (valueX.CalculateDifference() < 100 && valueY.CalculateDifference() < 100 && valueZ.CalculateDifference() > 140)
+            if (valueX.CalculateDifference() < 70 && valueY.CalculateDifference() < 70 && valueZ.CalculateDifference() > 120)
             {
-                return !valueZ.IsDirectionUp(); //erst Max, dann Min
+                //Min und Max jeweils weit genug von Median entfernt?
+                Console.WriteLine(("Median: " + valueZ.CalculateMedian() + " Min: " + valueZ.CalculateMin() + " Max: " + valueZ.CalculateMax() + "median-min: " + (valueZ.CalculateMedian() - valueZ.CalculateMin())));
+                if ((valueZ.CalculateMedian() - valueZ.CalculateMin()) > MedianDiffValue && valueZ.CalculateMax() - valueZ.CalculateMedian() > MedianDiffValue)
+                {
+
+                    return !valueZ.IsDirectionUp(); //erst Max, dann Min
+                }
+            }
+
+            return false;
+        }
+        private bool isDown()
+        {
+            if (valueX.CalculateDifference() < 70 && valueY.CalculateDifference() < 70 && valueZ.CalculateDifference() > 120)
+            {
+                //Min und Max jeweils weit genug von Median entfernt?
+                if (valueZ.CalculateMedian() - valueZ.CalculateMin() > MedianDiffValue && valueZ.CalculateMax() - valueZ.CalculateMedian() > MedianDiffValue)
+                {
+                    return valueZ.IsDirectionUp(); //erst Max, dann Min
+                }
             }
 
             return false;
@@ -65,11 +104,30 @@ namespace ChronosGestenerkennung.Gestures
 
         private bool isPush()
         {
-            if (valueX.CalculateDifference() < 70 && valueY.CalculateDifference() < 120 && valueZ.CalculateDifference() > 90)
+            if (valueX.CalculateDifference() < 70 && valueY.CalculateDifference() > 120 && valueZ.CalculateDifference() < 70)
             {
-                return valueY.IsDirectionUp(); 
+                return valueY.IsDirectionUp();
             }
             return false;
         }
+        private bool isLeft()
+        {
+            if (valueX.CalculateDifference() > 120 && valueY.CalculateDifference() < 70 && valueZ.CalculateDifference() < 70)
+            {
+                return !valueX.IsDirectionUp();
+            }
+            return false;
+        }
+
+        private bool isRight()
+        {
+            if (valueX.CalculateDifference() > 120 && valueY.CalculateDifference() < 70 && valueZ.CalculateDifference() < 70)
+            {
+                return valueX.IsDirectionUp();
+            }
+            return false;
+        }
+        
+
     }
 }
